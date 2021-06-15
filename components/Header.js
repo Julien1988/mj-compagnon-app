56c2a6b6bdef4d6c3c0d6243ba7c550d4c48a117
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-06-15 16:23:11 +0200

    bug

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/Image'
+import Image from 'next/Image';
 const Header = () => (
     <header className="header">
         <nav className="navbar">
