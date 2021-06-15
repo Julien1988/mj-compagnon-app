9516dd495f5f64e0b33548aeddf322caa690f7fb
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-06-15 16:11:44 +0200

    new footer infos

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,8 @@ const Footer = () => (
     <footer class='footer'>
         <div class='footer-wrapper'>
             <p class='footer__text'>Dev by Julien Broens</p>
+            <a href="mailto:julienbroens@gmail.com" class='footer__link email'>julienbroens@gmail.com</a>
+            <p class='footer__text'>Follow me on Twitter : DrV0dka </p>
         </div>
 
     </footer>
