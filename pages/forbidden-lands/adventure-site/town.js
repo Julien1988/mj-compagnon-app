b0a822d2c0362c6e28587df5a389b9d4276659a6
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-08-17 17:39:58 +0200

    add age of construction

--- pages/forbidden-lands/adventure-site/town.js
+++ pages/forbidden-lands/adventure-site/town.js
@@ -12,7 +12,7 @@ const Town = (data) => {
         setIsSubmitting(false)
       
     },[data]);
-    console.log(data[0])
+    console.log(data[1])
     return (
         <div className="container main-container">
             <h1>Génération d'un village</h1>
@@ -30,6 +30,10 @@ const Town = (data) => {
                             </Card.Header> 
                                 <div>
                                    <p>Habitants : {data[0].peoples}</p>
+                                </div>
+                                <div>
+                                    <p>Période de construction: {data[1].construction_period} ({data[1].age})</p>
+                                    
                                 </div>
                                       
                         
@@ -58,21 +62,22 @@ Town.getInitialProps = async () => {
     const getRandomArbitrary = (min, max) => {
     return Math.random() * (max - min) + min;
     }
-
+    // data array to return
     let dataArray = [];
+
     // get type of town
     let getTypeOfTown;
 
     const typeOfTown = Math.round(getRandomArbitrary(1, 6));
     console.log(typeOfTown)
     if (typeOfTown < 3) {
-        console.log("Avant-poste");
+       // console.log("Avant-poste");
         getTypeOfTown = 0;
     } else if (typeOfTown > 2 && typeOfTown < 6) {
-        console.log('Hameau')
+       // console.log('Hameau')
         getTypeOfTown = 1;
     } else {
-        console.log('Village');
+       // console.log('Village');
         getTypeOfTown = 2;
     }
 
@@ -81,6 +86,30 @@ Town.getInitialProps = async () => {
     const dataTown = await resTown.json();
 
     dataArray.push(dataTown[getTypeOfTown]);
+
+    // get age of town
+    let getAgeOfTown;
+    const ageOfTownDice = Math.round(getRandomArbitrary(11, 66));
+    console.log(ageOfTownDice)
+
+    // Api call Age Of Town
+    const resAgeOfTown = await fetch(CONST_URL+'/api/forbidden-lands/age-of-the-village');
+    const dataAgeOfTown = await resAgeOfTown.json();
+    if (ageOfTownDice < 17) {
+        getAgeOfTown = 0;
+    } else if (ageOfTownDice > 16 && ageOfTownDice < 26) {
+        getAgeOfTown = 1;
+    } else if (ageOfTownDice > 25 && ageOfTownDice < 57) {
+        getAgeOfTown = 2;
+    } else {
+        getAgeOfTown = 3;
+    }
+    dataArray.push(dataAgeOfTown[getAgeOfTown]);
+    
+    
+    // let num = 15;
+    // let n = num.toString();
+    // console.log(n)
     
 
     return dataArray
