fbaccb3ba0cfb1a58a5e4c566f0ddd68754143b2
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-08-20 16:11:56 +0200

    add inn name API

---
+++ pages/api/forbidden-lands/inn_name.js
@@ -0,0 +1,187 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default (req, res) => {
+    res.status(200).json({
+        0: {
+            first_word: "La lanterne",
+            second_word: "Décadent",
+            
+        },
+        1: {
+            first_word: "Le pourceau",
+            second_word: "Miniature",
+          
+        },
+        2: {
+            first_word: "La roue",
+            second_word: "Rouge",
+
+        },
+        3: {
+            first_word: "Le porche",
+            second_word: "Brumeux",
+
+        },
+        4: {
+            first_word: "Le moineau",
+            second_word: "Sanglant",
+
+        },
+        5: {
+            first_word: "La chèvre",
+            second_word: "Antique",
+
+        },
+        6: {
+            first_word: "La flèche",
+            second_word: "Doré",
+
+        },
+        7: {
+            first_word: "Le pot",
+            second_word: "Glacé",
+
+        },
+        8: {
+            first_word: "L’agneau",
+            second_word: "Rafraîchissant",
+
+        },
+        9: {
+            first_word: "La damoiselle",
+            second_word: "Serviable",
+
+        },
+        10: {
+            first_word: "L’homme",
+            second_word: "Assoiffé",
+
+        },
+        11: {
+            first_word: "Le dragon ",
+            second_word: "Arrogant",
+
+        },
+        12: {
+            first_word: "Le griffon",
+            second_word: "Fringant",
+
+        },
+        13: {
+            first_word: "Le sanglier",
+            second_word: "Joyeux",
+
+        },
+        14: {
+            first_word: "Le tonneau",
+            second_word: "Chantant",
+
+        },
+        15: {
+            first_word: "Le barde",
+            second_word: "Rusé",
+
+        },
+        16: {
+            first_word: "Le chien",
+            second_word: "Sautillant",
+
+        },
+        17: {
+            first_word: "Le cheval",
+            second_word: "Plaintif",
+
+        },
+        18: {
+            first_word: "La fillette ",
+            second_word: "Cupide",
+
+        },
+        19: {
+            first_word: "Le loup",
+            second_word: "Rond",
+
+        },
+        20: {
+            first_word: "L’ours",
+            second_word: "De feu",
+
+        },
+        21: {
+            first_word: "Le fantôme",
+            second_word: "Solitaire",
+
+        },
+        22: {
+            first_word: "Le rat",
+            second_word: "D’argent",
+
+        },
+        23: {
+            first_word: "Le vase",
+            second_word: "Noir",
+
+        },
+        24: {
+            first_word: "La chope",
+            second_word: "Mort",
+
+        },
+        25: {
+            first_word: "Le gobelet",
+            second_word: "Géant",
+
+        },
+        26: {
+            first_word: "L’aigle",
+            second_word: "Rugissant",
+
+        },
+        27: {
+            first_word: "Le corbeau",
+            second_word: "Guilleret",
+
+        },
+        28: {
+            first_word: "Le marteau",
+            second_word: "Fredonnant",
+
+        },
+        29: {
+            first_word: "La pique",
+            second_word: "Étriqué",
+
+        },
+        30: {
+            first_word: "La corneille",
+            second_word: "Obèse",
+
+        },
+        31: {
+            first_word: "Le druide",
+            second_word: "Charpenté",
+
+        },
+        32: {
+            first_word: "Le chevalier",
+            second_word: "Saoul",
+
+        },
+        33: {
+            first_word: "Le bandit",
+            second_word: "Suave",
+
+        }, 
+        34: {
+            first_word: "La laie",
+            second_word: "Tapageur",
+
+        },
+        35: {
+            first_word: "Le chasseur",
+            second_word: "Grognon",
+
+        },
+
+        
+     })
+  }
