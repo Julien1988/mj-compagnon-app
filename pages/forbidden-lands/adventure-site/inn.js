33841351ecf4276dbda6d71e915fcc8be2c86d5d
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-14 11:18:18 +0200

    add Inn random name and bool

--- pages/forbidden-lands/adventure-site/inn.js
+++ pages/forbidden-lands/adventure-site/inn.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import {useState} from 'react'
 import { Loader } from 'semantic-ui-react'
+import { CONST_URL } from '../../../constants';
 
-const AdventureSite = () => {
+const Inn = () => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const handleSubmit = () => {
         setIsSubmitting(true);
@@ -27,4 +28,59 @@ const AdventureSite = () => {
     )
 }
 
-export default AdventureSite;
+Inn.getInitialProps = async () => {
+    // utils fonct
+    const getRandomArbitrary = (min, max) => {
+    return Math.random() * (max - min) + min;
+    }
+    // data array to return
+    let dataArray = [];
+
+
+    // Api inn name
+    const resInNames = await fetch(CONST_URL+'/api/forbidden-lands/inn_name');
+    const dataInNames = await resInNames.json();
+
+    // get Inn name
+    let innName;
+    let getRandomDiceOne
+    let getRandomDiceTwo
+    const dubbleFirtName = Math.round(getRandomArbitrary(1, 2));
+    let dubbleFirstNameBool;
+        (dubbleFirtName == 1)
+            ? dubbleFirstNameBool = true 
+            : dubbleFirstNameBool = false
+    
+            switch (dubbleFirstNameBool) {
+                case true:
+                    // true
+                    console.log("true")
+                    getRandomDiceOne = Math.round(getRandomArbitrary(0, 35));
+                    getRandomDiceTwo = Math.round(getRandomArbitrary(0, 35));
+                    if (getRandomDiceOne == getRandomDiceTwo) {
+                        innName = dataInNames[getRandomDiceOne].first_word + ' ' + dataInNames[getRandomDiceTwo].second_word;
+                    }
+                    innName = (dataInNames[getRandomDiceOne].first_word + ' et ' + dataInNames[getRandomDiceTwo].first_word);
+                  break;
+               
+                default:
+                    //false
+                    console.log("false")
+                    getRandomDiceOne = Math.round(getRandomArbitrary(0, 35));
+                    getRandomDiceTwo = Math.round(getRandomArbitrary(0, 35));
+                    innName = dataInNames[getRandomDiceOne].first_word + ' ' + dataInNames[getRandomDiceTwo].second_word;
+                    
+            }
+    
+    console.log(innName);
+    
+    
+    //console.log(dataInNames[0].first_word)
+
+
+  
+    return dataArray
+
+}
+
+export default Inn;
