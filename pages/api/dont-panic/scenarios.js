0249f066e7dae0bd5c26d08b3390f0130b1da6e8
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-15 13:51:25 +0200

    creat new API for scearios

---
+++ pages/api/dont-panic/scenarios.js
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default (req, res) => {
+    res.status(200).json({
+        0: {
+            Theme: "Invasion",
+             event: ["Extraterrestres", "Zombies", "Orcs", "Démons", "Insectes géants",  "Clowns tueurs"]
+        },
+       
+          
+  
+        
+     })
+  }
--- pages/dont-panic/play.js
+++ pages/dont-panic/play.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { Loader, Card } from 'semantic-ui-react'
+import { CONST_URL } from './../../constants';
 
-const DontPanic = () => {
+const Play = (data) => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const handleSubmit = () => {
         setIsSubmitting(true);
     }
+    useEffect(() => {
+        setIsSubmitting(false)
+      
+    }, [data]);
     return (
         <div className='container main-container'>
             <h1>Don't Panic</h1>
@@ -14,14 +19,27 @@ const DontPanic = () => {
                 isSubmitting
                     ? <Loader active inline="centered"/>
                     : <div className="main-container__content">
-                        
+                         <Card>
+                            <Card.Content>
+                                <Card.Header>
+                                    <p>C'est partit !!!</p>
+                                        
+                                </Card.Header> 
+                                    <div>
+                                        <p className='little-margin-bottom'>toto</p>
+                                    </div>
+                                    
+                            
+                                    
+                                </Card.Content>     
+                            </Card>
                         <ul className="list-container">
                             <li className="list">
                              <p className="list list-color">Ce jeu est une création originale de Jentey Guillaume</p>   
                             </li>
                             <li className="list">
                             <Link href="/dont-panic/play">
-                                <a onClick={handleSubmit} className="list-link">Jouer au jeu</a>
+                                <a onClick={handleSubmit} className="list-link">Nouvelle partie</a>
                             </Link>
                             </li>
                             <li className="list">
@@ -37,5 +55,24 @@ const DontPanic = () => {
       </div>
     )
 }
+Play.getInitialProps = async () => {
+    // utils fonct
+    const getRandomArbitrary = (min, max) => {
+    return Math.random() * (max - min) + min;
+    }
+    // data array to return
+    let dataArray = [];
 
-export default DontPanic;
+
+    // Api scenarios 
+    const resScenarios = await fetch(CONST_URL+'/api/dont-panic/scenarios');
+    const dataScenarios = await resScenarios.json();
+
+    
+
+  
+    return dataArray
+
+}
+
+export default Play;
