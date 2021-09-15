60f84c76e823c65c053627ecc1b57abd3aefe754
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-15 15:42:08 +0200

    add bool for climax with useEffect hook

--- pages/dont-panic/play.js
+++ pages/dont-panic/play.js
@@ -7,6 +7,10 @@ const Play = (data) => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [diceResult, setDiceResult] = useState("Lancer les dés")
     const [isDiceSubmitting, setIsDiceSubmitting] = useState(false)
+    // counter default 1O
+    const [playCounter, setPlayCounter] = useState(1);
+    const [counterOn, setCounterOn] = useState(true)
+    const [gameIsOn, setGameIsOn] = useState(true)
     const [rulesContent, setRulesContent] = useState('');
     const [firstDice, setFirstDice] = useState();
     const [secondDice, setSecondDice] = useState();
@@ -18,17 +22,18 @@ const Play = (data) => {
     let getFirstDice;
     let getSecondDice;
     const dicesSubmit = () => {
-        
+        setCounterOn(true)
         getFirstDice = Math.round(getRandomArbitrary(1, 6))
         setFirstDice(getFirstDice)
         getSecondDice = Math.round(getRandomArbitrary(1, 6))
         setSecondDice(getSecondDice)
         setDiceResult(getFirstDice + getSecondDice);
-        
+       
         
     }
 
     useEffect(() => {
+        
         if (diceResult != "Lancer les dés") {
             if (diceResult < 7 && firstDice != secondDice) {
                 setRulesContent("lea joueureuse raconte comment la situation dérape.")
@@ -40,13 +45,25 @@ const Play = (data) => {
 
                 setDiceResult("DOUBLE !!!" + firstDice + " " + secondDice)
                 setRulesContent("iel doit aussi clore la scène (qu’elle se finisse bien ou non)")
+                if (counterOn == true) {
+                    setCounterOn(false)
+                    setPlayCounter(playCounter - 1)
+                    
+
+                }
             }
+        }
+        if (playCounter < 1) {
+            setGameIsOn(false)
+            console.log(gameIsOn)
         }
         setIsDiceSubmitting(true)
     }, [dicesSubmit])
     
     const handleSubmit = () => {
         setIsSubmitting(true);
+        setPlayCounter(10)
+        setDiceResult("Lancer les dés")
     }
     useEffect(() => {
         setIsSubmitting(false)
@@ -61,40 +78,66 @@ const Play = (data) => {
                 isSubmitting
                     ? <Loader active inline="centered"/>
                     : <div className="main-container__content">
-                         <Card>
-                            <Card.Content>
-                                <Card.Header>
-                                    
-                                    <div>
-                                    <p>Scénario :</p>
-                                    <p className='little-margin-bottom'>{data[0]} - {data[1] }</p>
-                                    </div>
+                        <Card>
+                            {
+                                gameIsOn
+                                ? <Card.Content>
+                                    <Card.Header>
                                         
-                                </Card.Header>
-                                
-                                <div className='little-margin-bottom'>
-                                    <p className='little-margin-bottom'>Casting : Chacun.e décrit rapidement son personnage : nom, occupation, personnalité, quel.le acteurice joue son rôle… et tout ce qui vous semblera pertinent pour que chacun.e puisse se l’imaginer pendant la partie.</p>
-                                </div>
-                                <Card.Header>
-                                <div>
-                                        <Button onClick={dicesSubmit} className='little-margin-bottom'>{diceResult}</Button>
-                                </div>
-                                </Card.Header>
-                                {isDiceSubmitting
-                                ?  <div className='little-margin-bottom'>
-                                        <div className='little-margin-bottom'>
-                                            <p>{ rulesContent }</p>
+                                        <div>
+                                        
+                                        <p className='little-margin-bottom'> Compteur : {playCounter}</p>
                                         </div>
-                                    </div>
-                                :   <div className='little-margin-bottom'>
-                                     
-                                    </div>
-                                }
-                               
-                               
+                                            
+                                    </Card.Header>
+                                    <Card.Header>
+                                        
+                                        <div>
+                                        <p>Scénario :</p>
+                                        <p className='little-margin-bottom'>{data[0]} - {data[1] }</p>
+                                        </div>
+                                            
+                                    </Card.Header>
                                     
-                                </Card.Content>     
-                            </Card>
+                                    <div className='little-margin-bottom'>
+                                        <p className='little-margin-bottom'>Casting : Chacun.e décrit rapidement son personnage : nom, occupation, personnalité, quel.le acteurice joue son rôle… et tout ce qui vous semblera pertinent pour que chacun.e puisse se l’imaginer pendant la partie.</p>
+                                    </div>
+                                    <Card.Header>
+                                    <div>
+                                            <Button onClick={dicesSubmit} className='little-margin-bottom'>{diceResult}</Button>
+                                    </div>
+                                    </Card.Header>
+                                    {isDiceSubmitting
+                                    ?  <div className='little-margin-bottom'>
+                                            <div className='little-margin-bottom'>
+                                                <p>{ rulesContent }</p>
+                                            </div>
+                                        </div>
+                                    :   <div className='little-margin-bottom'>
+                                        
+                                        </div>
+                                    }   
+                                    </Card.Content>     
+                            
+                                : <Card.Content>
+                                        <Card.Header>
+                                            <div className='little-margin-bottom'>
+                                                <p>CLIMAX</p>
+                                            </div>
+                                        
+                                        </Card.Header>
+                                        <div className='little-margin-bottom'>
+                                        <p>Quand il n’y a plus de jeton au centre de la table : c’est le point dramatiquement culminant du film ! Tou.tes en même temps, les joueureuses racontent comment tout va trop mal ! On essaie de s’écouter et de rebondir les un.es sur les autres, mais il n’y a pas d’ordre : c’est le chaos, la fin du monde, le petit orteil sur le coin du pied de chaise ! Tout en racontant comment tout part en quenouille, chacun.e jette frénétiquement ses 2d6 ! Lea premier.e à obtenir un double 6 crie :</p>
+                                        </div>
+                                        <Card.Header>
+                                            <div>
+                                                <p>"DON'T PANIC !"</p>
+                                            </div>
+                                        
+                                        </Card.Header>
+                                </Card.Content>
+                             }
+                             </Card>
                         <ul className="list-container">
                             <li className="list">
                              <p className="list list-color">Ce jeu est une création originale de Jentey Guillaume</p>   
